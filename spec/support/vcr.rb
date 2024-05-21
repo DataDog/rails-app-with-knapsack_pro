ce304ac322da469a694bf8105675329a64489fca
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2024-05-21 17:03:46 +0200

    datadog-ci requires manual config of VCR and webmock (yet)

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,7 +4,12 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   config.ignore_localhost = true
+
+  config.ignore_request do |request|
+    # ignore all requests to datadoghq hosts
+    request.uri =~ /datadoghq/
+  end
 end
 
 require 'webmock/rspec'
-WebMock.disable_net_connect! # Needed to test https://github.com/KnapsackPro/knapsack_pro-ruby/pull/251
+WebMock.disable_net_connect!(:allow => /datadoghq/)
